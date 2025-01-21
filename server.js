8efaf4ff099fbb2e600f264a5ed951f5752970cf
danielraso7 <danielraso73@gmail.com>
2025-01-21 17:28:20 +0100

    initial commit

---
+++ server.js
@@ -0,0 +1,14 @@
+import express from "express";
+import posts from "./routes/posts.js";
+const port = process.env.PORT || 8000;
+
+const app = express();
+
+// Body parser middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// Routes
+app.use("/api/posts", posts);
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));
